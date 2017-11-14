30c25cf6a7f8e9cc24e1ce9e3d6216409de278b8
Thalyta Fabrine <thalyta.trindade@ccc.ufcg.edu.br>
2017-11-13 22:43:13 -0300

    atualizando

--- quizccc.cpp
+++ quizccc.cpp
@@ -9,7 +9,21 @@ int tamanho = 5;
 // Tabuleiro
 int quizccc[5][5];
 
-// Métodos
+// Quantidade de buracos
+int buracos;
+
+// Nivel do jogo
+int nivel;
+
+//Linha do tabuleiro
+int i;
+
+//Coluna do tabuleiro
+int j;
+
+//Quantidade de vidas que o jogador possui . No inicio do jogo, ele comeca com 3
+int vidas = 3;
+// Metodos
 
 // Boas vindas do jogo
 void textoInicial();
@@ -18,8 +32,42 @@ void textoFinal();
 // Exibe os temas que o jogador pode escolher
 void exibirTemas();
 void preencherTabuleiro();
-// Imprime o tabuleiro que exibe apenas as casas que ele já percorreu
+// Imprime o tabuleiro que exibe apenas as casas que ele jÃ¡ percorreu
 void imprimirTabuleiro(int tabuleiro[5][5]);
+
+// estrutura simples que representa cada casa de um tabuleiro
+struct casa {
+	int ehBuraco;
+	int foiPisado; // se o usuï¿½rio jï¿½ caiu nessa casa
+}
+
+//Define posicao dos buracos no tabuleiro
+void posicaoBuracos();
+
+//Define quantidade de buracos conforme o nÃ­vel de jogo
+void qtdBuracos();
+
+void qtdBuracos(){
+  if (nivel == 1){
+    buracos = 8;
+  }
+  if (nivel == 2){
+    buracos = 10;
+  }
+  if (nivel == 3){
+    buracos = 12;
+  }
+}
+
+void posicaoBuracos(){
+  i = rand() % 5 + 1;
+  j = rand() % 5 + 1;
+  if (quizccc[i][j] != 1){
+      quizccc[i][j] = 1;
+  }else{
+    posicaoBuracos();
+  }
+}
 
 int main () {
     textoInicial();
@@ -70,11 +118,11 @@ void imprimirTabuleiro(int tabuleiro[5][5]){
         printf("                              ");
         printf("|");
         for(int y = 0; y < tamanho; y++){
-            // se o valor é -1, significa que ele ja passou por essa posicao
+            // se o valor Ã© -1, significa que ele ja passou por essa posicao
             if(tabuleiro[x][y] == -1){
                 printf(" x |");
             }
-            // se o valor é 0, essa casa eh um buraco
+            // se o valor Ã© 0, essa casa eh um buraco
             else if(tabuleiro[x][y] == 0){
                 printf(" * |");
             }
@@ -87,3 +135,4 @@ void imprimirTabuleiro(int tabuleiro[5][5]){
         puts("---------------------");
     }
 }
+
