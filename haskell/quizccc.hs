54b1c9befab294c04277c6a2c2f2d1d25b6088e7
Thalyta Fabrine <thalyta.trindade@ccc.ufcg.edu.br>
2017-12-10 20:25:30 -0300

    Implementando funcao do menu

--- haskell/quizccc.hs
+++ haskell/quizccc.hs
@@ -13,6 +13,33 @@ incrementaScore x  y| x == 1 = y+50
      | x == 2 = y + 100
      | x == 3 = y + 150
 
+-- Inicialmente salvei tema e nivel dentro da funcao, precisamos ver como
+-- utilizar isso pras demais funcoes
+showMenu = do
+    putStrLn "\n"
+    putStrLn "Indique o numero correspondente ao tema que voce deseja jogar:"
+    putStrLn "\n"
+    putStrLn "1 - Animais\n"
+    -- Lembrando que falta criar perguntas para os temas seguintes,
+    -- e analisar a sugestao da prof de adicionar temas de PLP
+    putStrLn "2 - Comida\n"
+    putStrLn "3 - Esportes\n"
+    
+    tema <- getLine
+    
+    putStrLn "\n"
+    putStrLn "Indique o numero correspondente ao nivel desejado"
+    putStrLn "\n"
+    -- Tambem precisamos analisar como separar as perguntas por niveis
+    putStrLn "1 - Facil\n"
+    putStrLn "2 - Medio\n"
+    putStrLn "3 - Dificil\n"
+    
+    nivel <- getLine
+    
+    putStrLn "\n"
+    putStrLn "Excelente, agora vamos iniciar o jogo\n"
+
 textoInicial = do
     putStrLn "------------------------------------------------------------------------------"
     putStrLn "  ########  ####   ###  #### ##########      ########   ########   ########   "
