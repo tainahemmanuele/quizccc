338ea486d4cdf2baeb88e0183d8af6624e58bc26
tainahemmanuele <tainahemmanuele@gmail.com>
2017-12-16 20:45:04 -0300

    Correcao em problema na compilacao

--- haskell/quizccc.hs
+++ haskell/quizccc.hs
@@ -22,8 +22,8 @@ incrementaScore x  y| x == 1 = y+50
 
 decrementaLife :: Int -> Int
 decrementaLife x | x == 0 = -1
-     |otherwise x = x -1
-     
+     |otherwise  = x -1
+
 -- Inicialmente salvei tema e nivel dentro da funcao, precisamos ver como
 -- utilizar isso pras demais funcoes
 showMenu = do
