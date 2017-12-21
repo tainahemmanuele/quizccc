91e24ec54adf484185ea0985391d0e6c39599019
tainahemmanuele <tainahemmanuele@gmail.com>
2017-12-21 10:46:49 -0300

    Inicio da implementacao de perguntas nivel 1 do tema animais

--- haskell/quizccc.hs
+++ haskell/quizccc.hs
@@ -60,6 +60,15 @@ exibirPergunta i  nivel score vida lista = do
         putStrLn  ("Voce errou! A resposta correta eh a alternativa:" ++(alternativas (lista!!i)))
         putStrLn ("Quantidade de vidas: " ++show(vidaAtual))
 
+perguntasN1 :: Array Int Pergunta
+perguntasN1 = array (1,25) [(1, Pergunta {texto = "Ratos sentem cocegas. Essa afirmacao eh:", alternativas = "1.Verdadeira \n2.Falso", resposta = 1}), 
+    (2, Pergunta {texto = "Alguns anfibios saltam distancias que correspondem a no maximo 50 vezes o seu tamanho. Essa afirmacao eh:", alternativas = "1.Verdadeira \n2.Falso", resposta = 2}),
+    (3, Pergunta {texto = "As cabras tem sotaques diferentes. Essa afirmacao eh:", alternativas = "1.Verdadeira \n2.Falso", resposta = 1}),
+    (4, Pergunta {texto = "Quantas picadas um Pica-Pau da por minuto?", alternativas = "1.100 \n2.1000 \n3.200 \n4.500", resposta = 1}),
+    (5, Pergunta {texto = "Com quantos narizes as formigas sao equipadas?", alternativas = "1. 2 \n2. 5 \n3. 4 \n4. 0", resposta = 2}),
+    (6, Pergunta {texto = "Os Golfinhos dao nomes uns aos outros. Essa afirmacao eh:", alternativas = "1.Verdadeira \n2.Falso", resposta = 1}),
+    (7, Pergunta {texto = "Qual animal pode correr mais rapido do que um cavalo?", alternativas = "1.Gato \n2.Cachorro \n3.Barata \n4.Urso", resposta = 4}),
+    (8, Pergunta {texto = "Qual animal tem o coice mais poderoso?", alternativas = "1.Zebra \n2.Cavalo \n3.Girafa \n4.Vaca", resposta = 3})]
 -- Inicialmente salvei tema e nivel dentro da funcao, precisamos ver como
 -- utilizar isso pras demais funcoes
 showMenu = do
@@ -84,6 +93,6 @@ showMenu = do
     putStrLn "3 - Dificil\n"
     
     nivel <- getLine
-    
+
     putStrLn "\n"
     putStrLn "Excelente, agora vamos iniciar o jogo\n"
